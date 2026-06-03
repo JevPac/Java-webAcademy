31ecb9c425aec1e66d817e2652c91ba705dca974
jeveson pacifico <jevearaujojr@gmail.com>
2026-06-02 20:16:34 -0500

    preciso terminar o resto em casas não connsigo ver daqui

--- demo/src/main/java/br/com/webacademy/Main.java
+++ demo/src/main/java/br/com/webacademy/Main.java
@@ -1,11 +1,13 @@
 package br.com.webacademy;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int opcao;
+        Scanner sc = new Scanner(System.in);
         do {
             exibirMenu();
-            opcao = Integer.parseInt(console.readLine);
+            opcao = Integer.parseInt( sc.nextLine());
             switch (opcao) {
                 case 0 -> salvarProduto();
                 case 1 -> buscarproduto();
@@ -16,6 +18,7 @@ public class Main {
             default -> System.out.print("Opção invalida!");
 
             } 
+            sc.close();
         } while (opcao !=0 ); {
         }
     }
@@ -30,5 +33,32 @@ public class Main {
         System.out.println("5. Sair do sistema");
         System.out.print("Escoha uma opção");
 
+    }
+
+    private static void salvarProduto(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n###Criar Novo Produto###");
+        System.out.println("Nome:");
+        String nome = sc.next();
+        System.out.println("Quantidade: ");
+        int Quantidade = Integer.parseInt(sc.nextline());
+        System.out.println("Valor:");
+        double valor = Double.parseDouble(sc.nextline());
+        Produto produto = new Produto(nome, Quantidade, valor);
+        produtoDAO produtoDAO = new ProdutoDAO();
+        try {
+            produtoDAO.salvar(produto);
+            System.out.println("Produto criado com sucesso!");
+        } catch(exeption e) {
+            System.err.print(e.getMessage());
+        }
+        sc.close();
+    }
+    private static void buscarprodutoporID: {
+    Scanner sc = new Scanner(System.in)
+    System.out.println("\n###Buscar produto por ID###");
+    System.out.println("\nDigite o ID do produto: ");
+    long id = Long.parseLong(sc.nextLine());
+    ProdutoDAO produtoDAO = new produtoDAO();
     }
 }
